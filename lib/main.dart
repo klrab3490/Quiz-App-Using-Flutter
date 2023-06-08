1a7d38a6caf6544c821f3555d5d7617f58b3c48c
klrab3490 <rahulbushi69@gmail.com>
2023-06-08 13:02:04 +0530

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,15 @@ import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    const Quiz(),
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Quiz();
+  }
 }
